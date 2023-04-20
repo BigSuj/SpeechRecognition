3f4b04cf02e052aa854481c5e5cfd844d7e82aff
BigSuj <sujenk@gmail.com>
2023-04-20 19:48:27 +0000

    add class and data to json

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,10 +1,37 @@
 import os
 import pandas as pd
 import numpy as np
-import librosa
-import librosa.display
-import matplotlib.pyplot as plt
 import asyncio
+from pydub import AudioSegment
+import librosa
+from io import BytesIO
+
+import torch
+from torch.utils.data import Dataset
+
+class SpectrogramDataset(Dataset):
+    def __init__(self, df):
+        self.df = df
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        spectrogram = self.compute_spectrogram(row['audio_path'])
+        return {'spectrogram': spectrogram, 'label': row['gender']}
+
+    def compute_spectrogram(self, audio_path):
+        audio = AudioSegment.from_file(audio_path)
+        wav_file = BytesIO()
+        audio.export(wav_file, format='wav')
+        wav_file.seek(0)
+        with wav_file as file:
+            y, sr = librosa.load(file, sr=None, mono=True)
+        S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
+        S_dB = librosa.power_to_db(S, ref=np.max)
+        return S_dB
+
 
 # Load the TSV files
 validated_df = pd.read_csv('cv_corpus/en/validated.tsv', sep='\t')
@@ -20,21 +47,20 @@ df['audio_path'] = df['path'].apply(lambda x: os.path.join(audio_dir, x))
 
 print('Working on adding audio files to df...')
 
+# Split the dataframe into batches
+batch_size = 1000
+batch_dfs = np.array_split(df[:10], len(df[:10]) // batch_size + 1)
+
 # Define a function to process a batch of audio files
 async def process_batch(batch_df, statement):
     print(statement)
+    dataset = SpectrogramDataset(batch_df)
     spectrograms = []
-    for audio_path in batch_df['audio_path']:
-        y, sr = librosa.load(audio_path, sr=None, mono=True)
-        S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
-        S_dB = librosa.power_to_db(S, ref=np.max)
-        spectrograms.append(S_dB)
+    for i in range(len(dataset)):
+        spectrogram = dataset[i]['spectrogram']
+        spectrograms.append(spectrogram)
     batch_df['spectrogram'] = spectrograms
     return batch_df
-
-# Split the dataframe into batches
-batch_size = 1000
-batch_dfs = np.array_split(df, len(df) // batch_size + 1)
 
 # Process the batches asynchronously
 async def main():
@@ -48,3 +74,4 @@ async def main():
     return final_df
 
 final_df = asyncio.run(main())
+final_df.to_json('data.json')
